01f39e6a46352c3490a04ade3a0bf23e782d926c
Laurie Cai <laurie801@gmail.com>
2020-10-18 22:24:35 -0700

    Add modified rules for V2

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 /*
 GAME RULES:
-
+v1
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
+-----
+v2
+- A player loses his ENTIRE score when he rolls 2 6's in a row. After that, next player turn
+- Add an input field where players can set the winning score, so they could change the predefined score of 100
+- Add another dice to the game so there are 2 dices. The player loses current score if one of them is a 1.
 */
 
 // ---------- REFERENCE ----------
